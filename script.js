239b448d1b1dc53a411f7b085735a7772063247e
Jagadeesh-Chalagala <117630691+Jagadeesh-Chalagala@users.noreply.github.com>
2023-07-16 23:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let newTourists = touristSpots.map( (place) => {
+	const newPlaceArr = place.split(" ");
+	let articleLessString = ""
+	for( let word of newPlaceArr){
+		if( word !== 'an' && word !== 'An' && word !== 'the' && word !== 'The' && word !== 'A' && word !== 'a')
+		{
+			articleLessString += word + " ";
+		}
+	}
+	return articleLessString.trim();
+} )
+newTourists.sort()
+touristSpots = newTourists;
 
+let list = document.getElementsByClassName("myList");
+touristSpots.forEach( (place) => {
+    const liElement = document.createElement('li');
+    liElement.textContent=place;
+    list.appendChild(liElement);
+} )
